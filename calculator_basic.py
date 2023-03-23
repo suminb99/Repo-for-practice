1613f19ca5e5e3480acbb18c5a1aa90707d38182
misung-dev <128569095+misung-dev@users.noreply.github.com>
2023-03-23 17:45:20 +0900

    소웨이 과제
    calculator_basic

---
+++ calculator_basic.py
@@ -0,0 +1,8 @@
+num1 = int(input('Enter number 1: '))
+num2 = int(input('Enter number 2: '))
+print('Some arithmetic results with', num1, 'and', num2)
+print(num1, '+', num2, '=', num1 + num2)
+print(num1, '-', num2, '=', num1 - num2)
+print(num1, '*', num2, '=', num1 * num2)
+print(num1, '//', num2, '=', num1 // num2)
+print(num1, '/', num2, '=', num1 / num2)
